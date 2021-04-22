c1ae641f69407891de6d7512e2eccb4d09133980
jinwook-song <wlsdnr129@naver.com>
2021-04-22 20:16:06 +0900

    lecture 2.30 movies Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController], // take URl, and execute fn
-  providers: [AppService],
+  controllers: [MoviesController], // take URl, and execute fn
+  providers: [],
 })
 // Everything must be in AppModule
 export class AppModule {}
