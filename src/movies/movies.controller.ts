f888917ab0bd215e7d948218bead686621bd9e73
jinwook-song <wlsdnr129@naver.com>
2021-04-23 16:17:38 +0900

    lecture 2.5 DTOs and validation update movie

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create.movie.dto';
+import { UpdateMovieDto } from './dto/update.movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -23,7 +24,6 @@ export class MoviesController {
 
   @Get(':id')
   getOne(@Param('id') movieId: number): Movie {
-    console.log(typeof movieId);
     return this.moviesService.getOne(movieId);
   }
 
@@ -39,7 +39,7 @@ export class MoviesController {
 
   // update some resources
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 }
