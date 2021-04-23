8a786626b934347406f21fb88e29381d51dff1e7
jinwook-song <wlsdnr129@naver.com>
2021-04-23 16:33:38 +0900

    lecture 2.6 Modules and Dependency Injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController], // take URl, and execute fn
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController], // take URl, and execute fn
+  providers: [],
 })
 // Everything must be in AppModule
 export class AppModule {}
