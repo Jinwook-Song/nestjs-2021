afd06e28861fafe142c2898eaaf7c005041705d6
jinwook-song <wlsdnr129@naver.com>
2021-10-07 21:25:55 +0900

    lecture 3.2 get movies test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,8 +1,14 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
   let service: MoviesService;
+  let testMovie = {
+    title: 'test title',
+    year: 0,
+    genres: ['test'],
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,7 +22,31 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be 4', () => {
-    expect(2 + 2).toEqual(4);
+  describe('getAllMovies', () => {
+    it('should return an array', () => {
+      const result = service.getAllMovies();
+      expect(result).toBeInstanceOf(Array);
+    });
   });
+
+  describe('getOneMovie', () => {
+    it('should return a movie', () => {
+      service.createMovie(testMovie);
+      const movie = service.getOneMovie(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOneMovie(999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
+
+  it.todo('createMovie');
+  it.todo('deleteMovie');
+  it.todo('editMovie');
 });
